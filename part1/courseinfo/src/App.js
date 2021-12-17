6b5ff62eaac0cc02fb450301e5b1cf919051b73d
Dustin Lammiman <dustin@nosecreekweb.ca>
2021-12-16 18:32:21 -0700

    Update to 1.1

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -8,9 +8,17 @@ const Header = (props) => {
 
 const Content = (props) => {
   return (
-    <p>
-      {props.part} {props.exercise}
-    </p>
+    <div>
+      <p>
+        {props.part1} {props.exercise1}
+      </p>
+      <p>
+        {props.part2} {props.exercise2}
+      </p>
+      <p>
+        {props.part3} {props.exercise3}
+      </p>
+    </div>
   )
 }
 
@@ -32,9 +40,7 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content part={part1} exercise={exercises1} />
-      <Content part={part2} exercise={exercises2} />
-      <Content part={part3} exercise={exercises3} />
+      <Content part1={part1} exercise1={exercises1} part2={part2} exercise2={exercises2} part3={part3} exercise3={exercises3} />
       <Total total={exercises1+exercises2+exercises3} />
     </div>
   )
