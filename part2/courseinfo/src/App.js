731e2a14a9ef73b87ee6fe612655563b98527ef6
Dustin Lammiman <dustin@nosecreekweb.ca>
2021-12-29 20:38:12 -0700

    2.1

---
+++ part2/courseinfo/src/App.js
@@ -0,0 +1,69 @@
+import React, { useState } from 'react'
+
+const Header = ({ title }) => {
+  return (
+    <h1>{title}</h1>
+  )
+}
+
+const Total = ({ course }) => {
+  const sum = course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises
+  return(
+    <p>Number of exercises {sum}</p>
+  ) 
+}
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.part.name} {props.part.exercises}
+    </p>    
+  )
+}
+
+const Content = ({parts}) => {
+  return (
+    <div>
+      {parts.map(part => 
+        <Part part={part} key={part.id} />
+      )}
+    </div>
+  )
+}
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header title={course.name} />
+      <Content parts={course.parts} />
+    </div>
+  )
+}
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return <Course course={course} />
+}
+
+export default App
