9884151c301c460f96ad8d2e7b5983c06dbe4b0f
Dustin Lammiman <dustin@nosecreekweb.ca>
2021-12-30 10:20:49 -0700

    2.6

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react'
+
+const Person = ({name}) => {
+  return (
+    <p>{name}</p>
+  )
+}
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+  
+  const addPerson = (event) => {
+    event.preventDefault()
+    setPersons(persons.concat({"name": newName}))
+    setNewName('')
+  }
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map(person => <Person name={person.name} key={person.name} />)}
+    </div>
+  )
+}
+
+export default App
