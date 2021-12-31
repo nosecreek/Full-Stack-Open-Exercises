db6e01530bd71809916d2760747039c384e9485b
Dustin Lammiman <dustin@nosecreekweb.ca>
2021-12-31 09:50:07 -0700

    2.9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -6,12 +6,24 @@ const Person = ({person}) => {
   )
 }
 
+const Persons = ({persons, search}) => {
+  const personsToShow = search ? persons.filter(value => value.name.toLowerCase().indexOf(search.toLowerCase()) !== -1) : persons
+  
+  return (
+    personsToShow.map(person => <Person person={person} key={person.name} />)
+  )
+}
+
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', phone: "040-123456" }
+    { name: 'Arto Hellas', phone: "040-123456" },
+    { name: 'Ada Lovelace', phone: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', phone: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', phone: '39-23-6423122', id: 4 }
   ]) 
   const [newName, setNewName] = useState('')
   const [newPhone, setNewPhone] = useState('')
+  const [searchInput, setSearchInput] = useState('')
   
   const addPerson = (event) => {
     event.preventDefault()
@@ -34,9 +46,15 @@ const App = () => {
     setNewPhone(event.target.value)
   }
 
+  const handleSearchChange = (event) => {
+    setSearchInput(event.target.value)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
+        <p>Filter shown with <input value={searchInput} onChange={handleSearchChange} /></p>
+      <h2>Add New</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
@@ -49,7 +67,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => <Person person={person} key={person.name} />)}
+      <Persons persons={persons} search={searchInput} />
     </div>
   )
 }
