077bb5aac442131e3b86ad37c2533f1dcc63a169
Dustin Lammiman <dustin@nosecreekweb.ca>
2022-02-04 11:11:43 -0700

    2.2b

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
 const blogSchema = new mongoose.Schema({
   title: String,
--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,6 +1,8 @@
 require('dotenv').config()
 const app = require('./app')
 const http = require('http')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
 const server = http.createServer(app)
 
