6def833b8d52166dd30872fc326436c2fd321531
Dustin Lammiman <dustin@nosecreekweb.ca>
2022-02-16 21:35:31 -0700

    4.22

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -3,6 +3,7 @@ const { response } = require('express')
 const jwt = require('jsonwebtoken')
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const middleware = require('../utils/middleware')
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', {username: 1, name: 1, id: 1})
@@ -18,14 +19,10 @@ blogsRouter.get('/:id', async (request, response) => {
   }
 })
   
-blogsRouter.post('/', async (request, response) => {
+blogsRouter.post('/', middleware.userExtractor, async (request, response) => {
 
   const body = request.body
-  const decodedToken = jwt.verify(request.token, process.env.SECRET)
-  if(!decodedToken.id) {
-    return response.status(401).json({error: 'token missing or invalid'})
-  }
-  const user = await User.findById(decodedToken.id)
+  const user = await User.findById(request.user)
 
   const blog = new Blog({
     title: body.title,
@@ -40,15 +37,10 @@ blogsRouter.post('/', async (request, response) => {
   response.status(201).json(result)
 })
 
-blogsRouter.delete('/:id', async (request, response) => {
-  
-  const decodedToken = jwt.verify(request.token, process.env.SECRET)
-  if(!decodedToken.id) {
-    return response.status(401).json({error: 'token missing or invalid'})
-  }
+blogsRouter.delete('/:id', middleware.userExtractor, async (request, response) => {
   const blog = await Blog.findById(request.params.id)
 
-  if(blog.user.toString() === decodedToken.id.toString()) {
+  if(blog.user.toString() === request.user.toString()) {
     const result = await Blog.findByIdAndRemove(request.params.id)
     response.status(204).end()
   } else {
--- part4/bloglist/utils/middleware.js
+++ part4/bloglist/utils/middleware.js
@@ -13,6 +13,17 @@ const tokenExtractor = (request, response, next) => {
   next()
 }
 
+const userExtractor = (request, response, next) => {
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
+  if(!decodedToken.id) {
+    return response.status(401).json({error: 'token missing or invalid'})
+  }
+
+  request.user = decodedToken.id
+  
+  next()
+}
+
 const errorHandler = (error, request, response, next) => {
   logger.error(error.message)
 
@@ -25,4 +36,4 @@ const errorHandler = (error, request, response, next) => {
   next(error)
 }
 
-module.exports = { tokenExtractor, errorHandler }
+module.exports = { tokenExtractor, userExtractor, errorHandler }
