bf1cb4a1f9eb35849fa48fb0b99b117e4bb45432
Dustin Lammiman <dustin@nosecreekweb.ca>
2022-03-06 16:19:54 -0700

    5.10

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -64,7 +64,7 @@ const App = () => {
       <div>
         <h2>blogs</h2>
         {blogs.map(blog =>
-          <Blog key={blog.id} blog={blog} />
+          <Blog key={blog.id} blog={blog} user={user} setMessage={setMessage} blogs={blogs} setBlogs={setBlogs} />
         )}
       </div>
       <br /><br />
--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,12 +1,13 @@
 import { useState } from "react"
 import blogService from '../services/blogs'
 
-const Blog = ({blog}) => {
+const Blog = ({blog, user, setMessage, blogs, setBlogs}) => {
   const [visible, setVisible] = useState(false)
   
   const hideWhenVisible = { display: visible ? 'none' : '' }
   const showWhenVisible = { display: visible ? '' : 'none' }
-
+  const isOwned = (blog.user ? blog.user.username : '') === user.username
+    
   const toggleVisibility = () => {
     setVisible(!visible)
   }
@@ -16,6 +17,16 @@ const Blog = ({blog}) => {
     await blogService.update(blog.id,blog)
     setVisible(visible+1)
   }
+
+  const handleDelete = async (blog) => {
+    if(window.confirm(`Delete ${blog.title} by ${blog.author}?`)) {
+      await blogService.deleteBlog(blog.id)
+      setMessage('Blog Deleted')
+      setBlogs(blogs.filter(b => b.id !== blog.id))
+    }
+  }
+
+  const deleteButton = isOwned ? <><br /><button onClick={()=>handleDelete(blog)}>delete</button></> : ''
 
   const blogStyle = {
     paddingTop: 10,
@@ -35,6 +46,7 @@ const Blog = ({blog}) => {
         {blog.url}<br />
         {blog.likes} <button onClick={()=>handleLike(blog)}>like</button><br />
         {blog.author}
+        {deleteButton}
       </div>
     </div>  
   )
