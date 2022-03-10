2a1c0a69cc124275843a87e8c25405dc3087835b
Dustin Lammiman <dustin@nosecreekweb.ca>
2022-03-09 17:56:17 -0700

    5.14

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -1,33 +1,42 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
-test('renders author and title', () => {
-  const blog = {
-    author: 'Dustin',
-    title: 'Test Blog 1',
-    url: 'http://www.myblog.com/1',
-    likes: 5
-  }
+describe('Test that blogs display the correct elements' , () => {
+  let container
 
-  const { container } = render(<Blog blog={blog} />)
-  const element = container.querySelector('.blog')
-  expect(element).toHaveTextContent('Test Blog 1')
-  expect(element).toHaveTextContent('Dustin')
-})
+  beforeEach(() => {
+    const blog = {
+      author: 'Dustin',
+      title: 'Test Blog 1',
+      url: 'http://www.myblog.com/1',
+      likes: 5
+    }
 
-test('does not render url or likes', () => {
-  const blog = {
-    author: 'Dustin',
-    title: 'Test Blog 1',
-    url: 'http://www.myblog.com/1',
-    likes: 5
-  }
+    container = render(<Blog blog={blog} />).container
+  })
 
-  const { container } = render(<Blog blog={blog} />)
-  let element = screen.queryByText('myblog.com')
-  expect(element).toBeNull()
-  element = screen.queryByText('5')
-  expect(element).toBeNull()
+  test('renders author and title', () => {
+    const element = container.querySelector('.blog')
+    expect(element).toHaveTextContent('Test Blog 1')
+    expect(element).toHaveTextContent('Dustin')
+  })
+
+  test('does not render url or likes', () => {
+    let element = screen.queryByText('myblog.com')
+    expect(element).toBeNull()
+    element = screen.queryByText('5')
+    expect(element).toBeNull()
+  })
+
+  test('clicking show displays url and likes', async () => {
+    const button = screen.getByText('view')
+    userEvent.click(button)
+
+    const element = container.querySelector('.blog')
+    expect(element).toHaveTextContent('Test Blog 1')
+    expect(element).toHaveTextContent('Dustin')
+  })
 })
