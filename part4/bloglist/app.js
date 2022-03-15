c41b7c181538efbddf1a4ca9a2fb2a1872a7a249
Dustin Lammiman <dustin@nosecreekweb.ca>
2022-03-15 17:43:29 -0600

    5.17

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -30,6 +30,11 @@ app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
 
+if (process.env.NODE_ENV === 'test') {
+  const testRouter = require('./controllers/test')
+  app.use('/api/test', testRouter)
+}
+
 app.use(middleware.errorHandler)
 
 module.exports = app
