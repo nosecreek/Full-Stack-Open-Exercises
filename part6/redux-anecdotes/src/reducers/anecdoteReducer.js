f8291e6a42e52bd8bf4cc548e097edfa3c5e32bc
Dustin Lammiman <dustin@nosecreekweb.ca>
2022-03-21 10:25:47 -0600

    6.3

--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -23,6 +23,21 @@ const reducer = (state = initialState, action) => {
   console.log('state now: ', state)
   console.log('action', action)
 
+  switch (action.type) {
+    case 'VOTE':
+    const id = action.data.id
+    const anecdoteToChange = state.find(a => a.id === id)
+    const changedAnecdote = {
+      ...anecdoteToChange,
+      votes: anecdoteToChange.votes + 1
+    }  
+    return state.map(a =>
+      a.id !== id ? a : changedAnecdote
+    )
+    
+    default: return state
+  }
+
   return state
 }
 
