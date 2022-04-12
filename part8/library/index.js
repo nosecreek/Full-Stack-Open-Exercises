6426962a2713375b2b490b7e0112230ac3ab883e
Dustin Lammiman <dustin@nosecreekweb.ca>
2022-04-12 17:52:30 -0600

    8.2

--- part8/library/index.js
+++ part8/library/index.js
@@ -85,9 +85,17 @@ let books = [
 ];
 
 const typeDefs = gql`
+  type Books {
+    title: String!
+    published: Int!
+    author: String!
+    genres: [String]
+    id: ID!
+  }
   type Query {
     bookCount: Int!
     authorCount: Int!
+    allBooks: [Books!]!
   }
 `;
 
@@ -95,6 +103,7 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
+    allBooks: () => books,
   },
 };
 
